a59670f469da7dfc674f83acc7377101b4f1d39f
hameedshk <kshameed7@gmail.com>
2020-06-18 12:54:47 +0530

    added content

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  providers: [DatePipe]
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  age: number;
+  birthdate: Date = new Date('08-10-1992');
+  constructor(public datepipe: DatePipe) { }
 
   ngOnInit() {
+    let timeDiff = Math.abs(Date.now() - this.birthdate.getTime());
+    this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365.25);
   }
 
 }

bc143848ddfa56d5b78f1202a7417632766bbc43
hameedshk <kshameed7@gmail.com>
2020-06-27 22:18:43 +0530

    ngx-translate

--- src/app/profile/profile/profile.component.ts
+++ src/app/profile/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DatePipe } from '@angular/common';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile',
@@ -10,11 +11,22 @@ import { DatePipe } from '@angular/common';
 export class ProfileComponent implements OnInit {
   age: number;
   birthdate: Date = new Date('08-10-1992');
-  constructor(public datepipe: DatePipe) { }
+  constructor(public datepipe: DatePipe,private translate: TranslateService) { 
+    translate.setDefaultLang('en');
+  }
+
+  
+  useLanguage(language: string) {
+    this.translate.use(language);
+  }
 
   ngOnInit() {
     let timeDiff = Math.abs(Date.now() - this.birthdate.getTime());
     this.age = Math.floor((timeDiff / (1000 * 3600 * 24)) / 365.25);
+  }
+
+  linksResolve(link:string){
+    
   }
 
 }
